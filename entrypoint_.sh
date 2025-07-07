338c6e149fd46fb9b5c7992d009284019aec691f
DESKTOP-G2BN01K\scoob <scooby.00@mail.ru>
2025-07-07 07:12:35 +0300

    doker up

--- entrypoint_.sh
+++ entrypoint_.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 set -e
 
-# Проверка и инициализация БД
 DB_FILE="/app/data/currency_data.db"
+
+mkdir -p /app/data
+
 if [ ! -f "$DB_FILE" ]; then
     echo "Database file not found. Creating new database..."
-    touch "$DB_FILE"
     sqlite3 "$DB_FILE" "CREATE TABLE IF NOT EXISTS currencies (id INTEGER PRIMARY KEY, code TEXT, rate REAL, date TEXT);"
 fi
 
-# Устанавливаем правильные права на БД
 chmod 660 "$DB_FILE"
 
-# Запуск приложения
 case "$1" in
     web)
         echo "Starting web server"
